3c560ed62355d9873913255a94f4810fda1899ba
sofojedal <111465717+sofojedal@users.noreply.github.com>
2023-03-31 17:55:07 -0500

    Update View.java

--- View.java
+++ View.java
@@ -17,11 +17,11 @@ public class View {
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 // Dibujar la imagen de la X en el centro del panel
-                g.drawImage(new ImageIcon("C:\\Users\\golden\\Downloads\\src\\imagenes\\freezer.jpg").getImage(), getWidth() / 2 - 16, getHeight() / 2 - 16, null);
+                g.drawImage(new ImageIcon("C:\\Users\\golden\\Documents\\laura\\x.png").getImage(), getWidth() / 2 - 16, getHeight() / 2 - 16, null);
 
                 // Dibujar la imagen pequeña en posiciones aleatorias
                 Random random = new Random();
-                for (int i = 0; i < 8000000; i++) {
+                for (int i = 0; i < 800; i++) {
                     int x = random.nextInt(getWidth() - smallImage.getIconWidth());
                     int y = random.nextInt(getHeight() - smallImage.getIconHeight());
                     g.drawImage(smallImage.getImage(), x, y, null);
@@ -30,7 +30,7 @@ public class View {
         };
 
         // Cargar la imagen pequeña
-        smallImage = new ImageIcon("C:\\Users\\golden\\Downloads\\src\\imagenes\\goku.jpg");
+        smallImage = new ImageIcon("C:\\Users\\golden\\Documents\\laura\\gato.png");
 
         frame.add(panel);
         frame.setVisible(true);
