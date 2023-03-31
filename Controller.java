e4a80c70e731d7f5520d7e14a2efb1e5cc86c9fe
sofojedal <111465717+sofojedal@users.noreply.github.com>
2023-03-31 17:40:13 -0500

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,9 @@
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+}
